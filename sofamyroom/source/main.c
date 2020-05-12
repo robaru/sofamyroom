8fb1cbf8e83fc72c2f1ac8fc1529161e5dfe7428
robaru <barumerli@dei.unipd.it>
2020-05-12 17:27:18 +0200

    headers removed

--- sofamyroom/source/main.c
+++ sofamyroom/source/main.c
@@ -1,25 +1,7 @@
 /*********************************************************************//**
  * @file main.c
  * @brief Basic room simulator in C
- **********************************************************************//*
- * Author: Steven Schimmel, stevenmschimmel@gmail.com
- * Copyright 2009, University of Zurich
- *************************************************************************
- * This file is part of ROOMSIM.
- *
- * ROOMSIM is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version. 
- *
- * ROOMSIM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ROOMSIM. If not, see <http://www.gnu.org/licenses/>.
- *************************************************************************/
+ **********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
