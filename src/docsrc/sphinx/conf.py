d8b70bde325a2843d0128de55475ba0de0664d0e
robaru <barumerli@dei.unipd.it>
2020-06-12 18:26:15 +0200

    sphinx conf updated to parse also markdown

--- src/docsrc/sphinx/conf.py
+++ src/docsrc/sphinx/conf.py
@@ -27,7 +27,8 @@ author = 'Roberto Barumerli Daniele Bianchi'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	"breathe"
+	"breathe",
+	"recommonmark"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.nojekyll']
 
 
 # -- Options for HTML output -------------------------------------------------
